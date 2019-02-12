627c438036ff1bc60387b93bdbc598d7104bee8e
U_35007842 <U_35007842@PC2>
2019-02-12 10:19:28 +0000

    paso 3 y 4

--- src/ut4pruebasintesisalbertogarciafdez/Calculadora.java
+++ src/ut4pruebasintesisalbertogarciafdez/Calculadora.java
@@ -8,5 +8,30 @@ public class Calculadora {
     private int operador1;
     private int operador2;
     private int operacion;
+
+    public int getOperador1() {
+        return operador1;
+    }
+
+    public int getOperador2() {
+        return operador2;
+    }
+
+    public int getOperacion() {
+        return operacion;
+    }
+
+    public void setOperador1(int operador1) {
+        this.operador1 = operador1;
+    }
+
+    public void setOperador2(int operador2) {
+        this.operador2 = operador2;
+    }
+
+    public void setOperacion(int operacion) {
+        this.operacion = operacion;
+    }
     
+        
 }
