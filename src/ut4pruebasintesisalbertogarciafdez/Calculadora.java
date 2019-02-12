89d4b7ce6e6a505c4f124313111d37dcd752662b
U_35007842 <U_35007842@PC2>
2019-02-12 10:17:58 +0000

    paso 1 y 2

---
+++ src/ut4pruebasintesisalbertogarciafdez/Calculadora.java
@@ -0,0 +1,12 @@
+package ut4pruebasintesisalbertogarciafdez;
+/**
+ *  ENTORNOS DE DESARROLLO (ETS) - ACTIVIDAD 2 (UT4)
+ * @author Alberto Julio García Fernández
+ */
+public class Calculadora {
+
+    private int operador1;
+    private int operador2;
+    private int operacion;
+    
+}
